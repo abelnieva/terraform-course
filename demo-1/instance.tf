119d3434e9ffc224ea026253b23284219d3f94d2
Abel Nieva <you@example.com>
2018-07-24 18:26:54 -0300

    esta fue la primera clase

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -1,4 +1,23 @@
 resource "aws_instance" "example" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
+  key_name      = "devops-training"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get -y update
+              sudo apt-get -y install nginx 
+              EOF
+}
+
+resource "aws_instance" "example1" {
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+  key_name      = "devops-training"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get -y update
+              sudo apt-get -y install apache2
+              EOF
 }
